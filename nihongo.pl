2d4c6c3e3bad9d4c0f774c36de028c34a5154efd
Aaron J Drake <aaron@myradius.io>
2023-01-16 12:46:24 -0500

    corrected lint warning

--- nihongo.pl
+++ nihongo.pl
@@ -12,9 +12,9 @@ sub nihongo()
         
         my $lesson = 'https://github.com/SethClydesdale/genki-study-resources/raw/master/resources/tools/wordlist_E-J/lessons-3rd/lesson-' . $i . '.xlsx';
         # print $lesson;
-        my $file = 'lesson'.$i.'.xlsx';
+        my $lesson_file = 'lesson'.$i.'.xlsx';
         # getstore($lesson, $file);
-        my $ff = File::Fetch->new(uri => $lesson);
+        my $ff = File::Fetch->new(uri => $lesson_file);
         my $file = $ff->fetch() or die $ff->error;
     }
 }
